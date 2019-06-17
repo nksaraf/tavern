c112b24c09a7fe9377d37028d0e68869290cec5b
Nikhil Saraf <nsaraf98@gmail.com>
2019-06-16 21:34:45 -0700

    patch 0.2.1

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,4 @@
 export { default as RequestHandler } from './request_handler';
 export { default as Parser } from './parser';
-export { default as ParserObject } from './parser_object';
 export { default as Logger } from './logger';
 export { default as ExpressAdapter } from './express_adapter';
