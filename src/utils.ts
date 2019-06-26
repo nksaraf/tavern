febe04df33e31de4f4c9d0d735391b06ff6c92b1
Nikhil Saraf <nsaraf98@gmail.com>
2019-06-25 18:15:00 -0700

    utils prettified and cleaned up

--- src/utils.ts
+++ src/utils.ts
@@ -1,59 +1,43 @@
 import _ from 'lodash';
 
-// export function isConstructor(object: any): boolean {
-//   try {
-//     new object();
-//   } catch (err) {
-//     if (err.message.indexOf('is not a constructor') >= 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-function isSimpleObject(value: any): boolean {
+function isObjectNotArray(value: any): boolean {
   return _.isObjectLike(value) && !_.isArrayLikeObject(value);
 }
 
 const typeCheckers: { [key: string]: (value?: any) => boolean } = {
-  object: isSimpleObject,
+  object: isObjectNotArray,
   number: _.isNumber,
   string: _.isString,
   message: ({ type, payload, ctx }) =>
-    _.isString(type) && isSimpleObject(payload) && isSimpleObject(ctx),
+    _.isString(type) && isObjectNotArray(payload) && isObjectNotArray(ctx),
   function: _.isFunction
 };
 
-export function throwTypeError(arg: string, value: any, type: string): never {
-  throw new TypeError(`${arg}=${value} is not of type: ${type}`);
-}
+const allTypesRepr = Object.keys(typeCheckers)
+  .map(a => `'${a}'`)
+  .join(', ');
 
-export function checkArgType<T>(
-  value: T,
-  type: string,
-  arg: string,
-  def?: T
-): T {
-  if (!(_.isString(type) && type in typeCheckers)) {
-    throw new Error(
-      `type=${type} is not one of ${Object.keys(typeCheckers)
-        .map(a => `'${a}'`)
-        .join(', ')}`
-    );
+export const typeError = (arg: string, value: any, type: string) =>
+  new TypeError(`${arg}=${value} is not of type: ${type}`);
+
+export function assertType<T>(value: T, type: string, arg: string, def?: T): T {
+  if (!_.isString(type) || !(type in typeCheckers)) {
+    throw typeError('type', type, `string {${allTypesRepr}}`);
+  } else if (!_.isString(arg)) {
+    throw typeError('arg', arg, 'string');
   }
 
-  if (!_.isString(arg)) throwTypeError('arg', arg, 'string');
+  if (typeCheckers[type](value)) {
+    return value;
+  }
 
   if (_.isUndefined(value)) {
     if (typeCheckers[type](def)) {
       return def as T;
     }
     throw new TypeError(`${arg} cannot be undefined (no default value)`);
-  } else if (typeCheckers[type](value)) {
-    return value;
-  } else {
-    throwTypeError(arg, value, type);
   }
 
+  throw typeError(arg, value, type);
   return def as T;
 }
