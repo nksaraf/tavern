882f13dfa1377c0e1c08d2cde0b366fcdcc8a526
Nikhil Saraf <nsaraf98@gmail.com>
2019-06-25 19:31:44 -0700

    cleaned up names

--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,42 @@
 import _ from 'lodash';
 
-function isObjectNotArray(value: any): boolean {
-  return _.isObjectLike(value) && !_.isArrayLikeObject(value);
-}
+const isNonArrayObject = (value: any) =>
+  _.isObjectLike(value) && !_.isArrayLikeObject(value);
 
-const typeCheckers: { [key: string]: (value?: any) => boolean } = {
-  object: isObjectNotArray,
+const validators: { [key: string]: (value?: any) => boolean } = {
+  object: isNonArrayObject,
   number: _.isNumber,
   string: _.isString,
   message: ({ type, payload, ctx }) =>
-    _.isString(type) && isObjectNotArray(payload) && isObjectNotArray(ctx),
+    _.isString(type) && isNonArrayObject(payload) && isNonArrayObject(ctx),
   function: _.isFunction
 };
 
-const allTypesRepr = Object.keys(typeCheckers)
-  .map(a => `'${a}'`)
-  .join(', ');
-
-export const typeError = (arg: string, value: any, type: string) =>
+export const argError = (arg: string, value: any, type: string) =>
   new TypeError(`${arg}=${value} is not of type: ${type}`);
 
-export function assertType<T>(value: T, type: string, arg: string, def?: T): T {
-  if (!_.isString(type) || !(type in typeCheckers)) {
-    throw typeError('type', type, `string {${allTypesRepr}}`);
+export function validate<T>(value: T, type: string, arg: string, def?: T): T {
+  if (!_.isString(type) || !(type in validators)) {
+    throw argError(
+      'type',
+      type,
+      `string {${Object.keys(validators).join(', ')}}`
+    );
   } else if (!_.isString(arg)) {
-    throw typeError('arg', arg, 'string');
+    throw argError('arg', arg, 'string');
   }
 
-  if (typeCheckers[type](value)) {
+  if (validators[type](value)) {
     return value;
   }
 
   if (_.isUndefined(value)) {
-    if (typeCheckers[type](def)) {
+    if (validators[type](def)) {
       return def as T;
     }
     throw new TypeError(`${arg} cannot be undefined (no default value)`);
   }
 
-  throw typeError(arg, value, type);
+  throw argError(arg, value, type);
   return def as T;
 }
